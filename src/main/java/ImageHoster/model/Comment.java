56aa674bc380b04d3a0559462f1eb39af72fe486
Apurva Jain <apurja97@in.ibm.com>
2021-02-17 17:44:42 +0530

    First Commit for Code

---
+++ src/main/java/ImageHoster/model/Comment.java
@@ -0,0 +1,75 @@
+package ImageHoster.model;
+
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="comments")
+public class Comment {
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    //@Column annotation specifies that the attribute will be mapped to the column in the database.
+    //Here the column name is explicitly mentioned as 'id'
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String text;
+
+
+    @Temporal(TemporalType.DATE)
+    @Column(name="createdDate")
+    private Date createdDate;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    //Below annotation indicates that the name of the column in 'comment' table referring the primary key in 'users' table will be 'user_id'
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    //Below annotation indicates that the name of the column in 'comment' table referring the primary key in 'image' table will be 'user_id'
+    @JoinColumn(name = "image_id")
+    private Image image;
+
+}
